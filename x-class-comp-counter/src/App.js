3aabac35a456040daa19329af08a9d28d30bd635
tejasvarpe12 <140579868+tejasvarpe12@users.noreply.github.com>
2024-04-28 10:46:29 +0530

    counter app using class component done

--- x-class-comp-counter/src/App.js
+++ x-class-comp-counter/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
+  }
 }
 
 export default App;
